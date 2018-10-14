d769687e1ef572cb41aa7d3da3238e2f5bed7852
Amr Reda <Amr@amrs-macbook-pro.local>
2018-10-14 21:01:29 +0200

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Amr Reda on 10/14/18.
+//  Copyright © 2018 Amr Reda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
